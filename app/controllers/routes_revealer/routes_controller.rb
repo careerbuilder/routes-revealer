21ef06844a6e03ef6ea95f2d4d2d2c556b2081d3
Justin Roberts <justin.roberts@careerbuilder.com>
2015-08-18 09:32:12 -0400

    new routes reveal logic

--- app/controllers/routes_revealer/routes_controller.rb
+++ app/controllers/routes_revealer/routes_controller.rb
@@ -3,16 +3,7 @@ module RoutesRevealer
     skip_filter(*_process_action_callbacks.map(&:filter))
 
     def index
-      output = []
-      Rails.application.routes.routes.each do |r|
-        route_string = r.path.spec.to_s.sub('(.:format)', '')
-        unless ignore_route?(route_string)
-          output.push route_string if r.defaults[:controller]
-        end
-      end
-
-      output.push Rails.application.class.config.assets.prefix
-
+      output = map_routes(Rails.application.routes.routes).flatten.compact.uniq
       render json: output
     end
 
@@ -20,6 +11,29 @@ module RoutesRevealer
 
     def ignore_route?(route_string)
       route_string =~ /(\/[0-9]{3})|(^\/routes)|(^\/rails)/
+    end
+
+    def map_routes(routes, prepend='')
+      prepend = '' if prepend == '/'
+      route_hash_array(routes).map do |route_hash|
+        if route_hash[:reqs].include?("::Engine")
+          map_routes(Module.const_get(route_hash[:reqs]).routes.routes, route_hash[:path])
+        else
+          "#{prepend}#{route_hash[:path]}" unless ignore_route?(route_hash[:path])
+        end
+      end
+    end
+
+    def route_hash_array(routes)
+      routes.map do |route|
+        ActionDispatch::Routing::RouteWrapper.new(route)
+      end.map do |route|
+        {
+          path: route.path.sub('(.:format)', ''),
+          verb: route.verb,
+          reqs: route.reqs
+        }
+      end
     end
   end
 end
--- spec/controllers/routes_controller_spec.rb
+++ spec/controllers/routes_controller_spec.rb
@@ -3,29 +3,65 @@ require 'spec_helper'
 describe RoutesRevealer::RoutesController do
   describe '#index' do
 
-    let(:good_defaults) { { controller: true } }
-    let(:bad_defaults) { { controller: nil } }
-    let(:good_route1) { double("good_route1", path: double("path", spec: "/good/route1(.:format)"), defaults: good_defaults) }
-    let(:good_route2) { double("good_route2", path: double("path", spec: "/good/route2(.:format)"), defaults: good_defaults) }
-    let(:bad_route1) { double("bad_route1", path: double("path", spec: "/404(.:format)"), defaults: good_defaults) }
-    let(:bad_route2) { double("bad_route2", path: double("path", spec: "/rails(.:format)"), defaults: good_defaults) }
-    let(:bad_route3) { double("bad_route3", path: double("path", spec: "/routes(.:format)"), defaults: good_defaults) }
-    let(:bad_route4) { double("bad_route4", path: double("path", spec: "/(.:format)"), defaults: good_defaults) }
-    let(:bad_route5) { double("bad_route5", path: double("path", spec: "/asdf(.:format)"), defaults: bad_defaults) }
-    let(:routes) { [good_route1, good_route2, bad_route1, bad_route2, bad_route3, bad_route4] }
+    let(:this_engine_string) { "This::Engine" }
+    let(:that_engine_string) { "That::Engine" }
+
+    let(:route1) { double("route1", path: "/1", verb: "GET", reqs: "controller#1") }
+    let(:route2) { double("route2", path: "/2", verb: "POST", reqs: "controller#2") }
+    let(:route3) { double("route3", path: "/3", verb: "PUT", reqs: "controller#3") }
+    let(:route4) { double("route4", path: "/4", verb: "DELETE", reqs: "controller#4") }
+    let(:route5) { double("route5", path: "/", verb: "GET", reqs: this_engine_string) }
+    let(:route6) { double("route6", path: "/that", verb: "GET", reqs: that_engine_string) }
+    let(:route7) { double("route7", path: "/routes", verb: "HEAD", reqs: "controller#routes") }
+    let(:route8) { double("route7", path: "/rails", verb: "TRACE", reqs: "controller#routes") }
+
+    let(:this_route1) { double("this_route1", path: "/t1", verb: "RAMBO", reqs: "this_controller#1") }
+    let(:this_route2) { double("this_route2", path: "/t2", verb: "ALWAYS", reqs: "this_controller#2") }
+    let(:that_route1) { double("that_route1", path: "/1", verb: "SHOOTS", reqs: "that_controller#1") }
+    let(:that_route2) { double("that_route2", path: "/2", verb: "WALNUTS", reqs: "that_controller#2") }
+
+    let(:routes) { [route1, route2, route3, route4, route5, route6] }
+    let(:this_routes) { [this_route1, this_route2] }
+    let(:that_routes) { [that_route1, that_route2] }
+
+    let(:this_engine) { double("this_engine", routes: double("routes", routes: this_routes))}
+    let(:that_engine) { double("that_engine", routes: double("routes", routes: that_routes))}
+
+    let(:route_wrapper) { ActionDispatch::Routing::RouteWrapper }
 
     before do
       allow(Rails).to receive_message_chain(:application, :routes, :routes).and_return(routes)
       allow(Rails).to receive_message_chain(:application, :class, :config, :assets, :prefix).and_return("/assets")
+
+      allow(Module).to receive(:const_get).with(this_engine_string).and_return(this_engine)
+      allow(Module).to receive(:const_get).with(that_engine_string).and_return(that_engine)
+
+      allow(route_wrapper).to receive(:new).with(route1).and_return(route1)
+      allow(route_wrapper).to receive(:new).with(route2).and_return(route2)
+      allow(route_wrapper).to receive(:new).with(route3).and_return(route3)
+      allow(route_wrapper).to receive(:new).with(route4).and_return(route4)
+      allow(route_wrapper).to receive(:new).with(route5).and_return(route5)
+      allow(route_wrapper).to receive(:new).with(route6).and_return(route6)
+
+      allow(route_wrapper).to receive(:new).with(this_route1).and_return(this_route1)
+      allow(route_wrapper).to receive(:new).with(this_route2).and_return(this_route2)
+
+      allow(route_wrapper).to receive(:new).with(that_route1).and_return(that_route1)
+      allow(route_wrapper).to receive(:new).with(that_route2).and_return(that_route2)
+
       get :index
     end
 
     it { expect(response.status).to eq 200 }
-    it { expect(JSON.parse(response.body).length).to eq 4 }
-    it { expect(JSON.parse(response.body)[0]).to eq "/good/route1" }
-    it { expect(JSON.parse(response.body)[1]).to eq "/good/route2" }
-    it { expect(JSON.parse(response.body)[2]).to eq "/" }
-    it { expect(JSON.parse(response.body)[3]).to eq "/assets" }
+    it { expect(JSON.parse(response.body).length).to eq 8 }
+    it { expect(JSON.parse(response.body)[0]).to eq "/1" }
+    it { expect(JSON.parse(response.body)[1]).to eq "/2" }
+    it { expect(JSON.parse(response.body)[2]).to eq "/3" }
+    it { expect(JSON.parse(response.body)[3]).to eq "/4" }
+    it { expect(JSON.parse(response.body)[4]).to eq "/t1" }
+    it { expect(JSON.parse(response.body)[5]).to eq "/t2" }
+    it { expect(JSON.parse(response.body)[6]).to eq "/that/1" }
+    it { expect(JSON.parse(response.body)[7]).to eq "/that/2" }
 
   end
 end
